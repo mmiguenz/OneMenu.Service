32da5721f1537efdd81af0bf2ca881f0891e5351
matias <miguenz.matias@gmail.com>
2021-06-05 23:26:37 -0300

    dockerize

--- src/OneMenu.Service/Startup.cs
+++ src/OneMenu.Service/Startup.cs
@@ -55,12 +55,8 @@ namespace OneMenu.Service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OneMenu.Service v1"));
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "OneMenu.Service v1"));
             app.UseHttpsRedirection();
 
             app.UseRouting();
